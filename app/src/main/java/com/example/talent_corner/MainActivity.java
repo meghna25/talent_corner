eb6657cb7441d7b1828b6d2ed46737e96c7f7b76
Meghna <meghna25d@gmail.com>
2020-09-03 19:08:57 +0530

    Initial commit

--- app/src/main/java/com/example/talent_corner/MainActivity.java
+++ app/src/main/java/com/example/talent_corner/MainActivity.java
@@ -2,13 +2,30 @@ package com.example.talent_corner;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
-public class MainActivity extends AppCompatActivity {
 
+public class MainActivity extends AppCompatActivity
+{
+    Button btn;
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState)
+    {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        btn= (Button)findViewById(R.id.instruction);
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent i = new Intent(MainActivity.this, MainActivity2.class);
+                startActivity(i);
+            }
+        });
     }
+
+
 }
